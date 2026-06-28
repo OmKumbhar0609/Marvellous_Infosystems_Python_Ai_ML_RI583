7b37be98f0bb250b0c8e2ed5fda44ef7394e7c48
Om Kumbhar <omkumbhar0625@gmail.com>
2026-06-29 01:53:15 +0530

    Create assignment2_1.py

---
+++ assignment2_1.py
@@ -0,0 +1,10 @@
+# Write a program to display Value, Type and Memory Address of a variable.
+
+x = 10
+print("Value:", x)
+print("Type:", type(x))
+print("Memory Address:", id(x))
+
+o/p: Value: 10
+    Type: <class 'int'>
+    Memory Address: 140714714502344
