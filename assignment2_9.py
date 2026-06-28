37f9922ee934f1749fc364a79af0da30f1e328f4
Om Kumbhar <omkumbhar0625@gmail.com>
2026-06-29 02:02:59 +0530

    Create assignment2_9.py

---
+++ assignment2_9.py
@@ -0,0 +1,10 @@
+#Write a program to take user's name and age and display:
+#Hello <name>, you will turn <age+1> next year.
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello {name}, you will turn {age + 1} next year.")
+
+o/p: Enter your name: Om
+Enter your age: 22
+Hello Om, you will turn 23 next year.
